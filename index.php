b1d85f8324fd690e41a7e9da86e90f94a1ddc006
shinathorn <152172580+shinathorn@users.noreply.github.com>
2023-12-21 18:52:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
+<!DOCTYPE html>
 <html>
-    <body>
-        <h1>This is PHP</h1>
-    </body>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+    <?php
+        isset( $_POST['base'] ) ? $base = $_POST['base'] : $base = "";
+        if( !empty( 2 ) ) {
+            echo "<hr/>"; 
+            
+            echo "<b>สูตรคูณแม่ 2</b><br/><br/>";
+        }
+        for( $i=1; $i<=24; $i++ ) {
+            echo "2 x {$i} = ".(2*$i)."<br>";
+        }
+    ?>
+</body>
 </html>
