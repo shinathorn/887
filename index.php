e505ce64bbe81a525f4895b658f5d65ec0b0b82c
shinathorn <152172580+shinathorn@users.noreply.github.com>
2023-12-26 00:21:47 +0700

    work3

--- index.php
+++ index.php
@@ -1,29 +1,42 @@
-<meta charset="utf-8">
+<html>
+
+<head>
+    <meta charset="utf-8">
     <link rel="stylesheet" href="w3.css" />
 </head>
 
 <body>
-    <?php $multi_x = 2; ?>
-    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
-    <style>
-        .scroll {
-            width: 150px;
-            height: 265px;
-            border: 1px solid black; 
-            overflow:auto; 
-        }
-    </style>
-    <div class="scroll">
+    <center>
+        <?php $multi_x = 2; ?>
+        <header>
+            <h1>ตารางสูตรคูณแม่
+                <?php echo $multi_x ?>
+            </h1>
+        </header>
+        <style>
+            .scroll {
+                width: 361px;
+                height: 498px;
+                border: 1px solid black;
+                color: white;
+                overflow: auto;
+                font-family:Georgia, 'Times New Roman', Times, serif;
+                font-size: 20px;
+            }
+        </style>
+        <div class="scroll">
             <tr>
-            <?php
-            if( !empty($multi_x) ) {
-        for( $i=1; $i<=24; $i++ ) {
-            echo"<k>";
-            echo "$multi_x x {$i} = ".( $multi_x *$i)."</br><br>";
-            echo"</k>";
-        }
-    }
-    ?>       
-    </div>
+                <?php
+                if (!empty($multi_x)) {
+                    for ($i = 1; $i <= 24; $i++) {
+                        echo "<k>";
+                        echo "$multi_x x {$i} = " . ($multi_x * $i) . "</br><br>";
+                        echo "</k>";
+                    }
+                }
+                ?>
+        </div>
+    </center>
 </body>
+
 </html>
